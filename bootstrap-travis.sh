5440ed0efc1fa04e1ff6e0b01cac23bfc8805aa1
Braxton Huggins <braxton@pagodabox.com>
2016-02-02 13:32:14 -0700

    try checking the output of red ping to know if it works or not

--- bootstrap-travis.sh
+++ bootstrap-travis.sh
@@ -4,7 +4,7 @@ wget -O /tmp/libmsgxchng_1.0.0-1_amd64.deb https://s3.amazonaws.com/nanopack.nan
 wget -O /tmp/red_1.0.0-1_amd64.deb https://s3.amazonaws.com/nanopack.nanobox.io/deb/red_1.0.0-1_amd64.deb
 wget -O /tmp/redd_1.0.0-1_amd64.deb https://s3.amazonaws.com/nanopack.nanobox.io/deb/redd_1.0.0-1_amd64.deb
 
-sudo apt-get -y update
+sudo apt-get -qq update
 sudo apt-get -y install libmsgpack3 libuv0.10
 
 sudo dpkg -i /tmp/libbframe_1.0.0-1_amd64.deb
@@ -15,8 +15,6 @@ sudo dpkg -i /tmp/redd_1.0.0-1_amd64.deb
 sudo wget -O /etc/redd.conf https://raw.githubusercontent.com/nanopack/redd/master/redd.conf
 sudo mkdir -p /var/db/redd
 sudo redd /etc/redd.conf
-red ping
-until [[ $? -ne 0 ]]; do
+while [[ $(red ping) == "Error: Unable to connect to RED\n" ]]; do
 	sleep 1
-	red ping
 done
